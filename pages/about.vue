1dd5390a94d8d25c26d119d08141dc663035bbd4
Kanagat Zhumagaliev <zhumagalievkanagat@gmail.com>
2024-06-02 14:08:51 +0500

    about adaptivity

--- pages/about.vue
+++ pages/about.vue
@@ -1,45 +1,9 @@
 <template>
   <div class="max-w-[1300px] w-full mx-auto px-4">
-    <div>
-      <div class="flex items-center justify-between">
-        <nuxt-icon
-          name="01"
-          filled
-          class="text-[90px] cursor-pointer"
-          @click.prevent="$router.push('/')"
-        />
-        <div
-          class="text-[17px] text-[white] font-medium hidden md:flex items-center"
-        >
-          <nuxt-link to="/academy" class="inline-block px-3">Academy</nuxt-link>
-          <nuxt-link to="/products" class="inline-block px-3"
-            >Products</nuxt-link
-          >
-          <nuxt-link to="/community" class="inline-block px-3"
-            >Community</nuxt-link
-          >
-          <nuxt-link to="/company" class="inline-block px-3">Company</nuxt-link>
-        </div>
-        <div class="hidden md:flex items-center">
-          <button
-            class="border-[1px] border-solid text-[17px] border-[#606060] text-[white] rounded-[8px] px-3 py-2"
-            @click.prevent="$router.push({ name: 'auth-login' })"
-          >
-            Log In
-          </button>
-          <button
-            class="text-[black] text-[17px] bg-[#D9D9D9] px-3 py-2 ml-3 rounded-[8px]"
-            @click.prevent="$router.push({ name: 'auth-register' })"
-          >
-            Sign Up
-          </button>
-        </div>
-        <nuxt-icon name="02" filled class="block md:hidden" />
-      </div>
-    </div>
-    <div class="text-white pt-[80px]">
-      <h1 class="text-center text-[48px] font-semibold mb-6">ABOUT US</h1>
-      <p class="text-center text-[24px] font-medium">
+    <NavigationBar />
+    <div class="text-white md:pt-[80px] pt-[40px]">
+      <h1 class="text-center md:text-[48px] text-[36px] font-semibold mb-6">ABOUT US</h1>
+      <p class="text-center md:text-[24px] text-[18px] font-medium">
         Welcome to the LA United Football Academy — your first step towards big
         football! We are the leading academy in the region, providing
         high-quality training and development of young talents. Our academy was
@@ -52,12 +16,12 @@
       </div>
       <div>
         <h1
-          class="text-[48px] font-bold text-center mb-5 text-[white] uppercase"
+          class="md:text-[48px] text-[36px] font-bold text-center mb-5 text-[white] uppercase"
         >
           TROPHIE HALL
         </h1>
         <div
-          class="grid grid-cols-3 gap-4 max-w-[1000px] mx-auto text-[23px] font-medium text-center"
+          class="grid md:grid-cols-3 md:gap-4 gap-10 max-w-[1000px] mx-auto text-[23px] font-medium text-center"
         >
           <div class="flex items-center flex-col justify-end">
             <img src="@/assets/images/kubok-2.png" alt="" class="mb-4" />
@@ -82,9 +46,9 @@
           </div>
         </div>
       </div>
-      <div class="my-[100px]">
+      <div class="md:my-[100px] my-[40px]">
         <h1
-          class="text-[48px] font-bold text-center mb-5 text-[white] uppercase"
+          class="md:text-[48px] text-[36px] font-bold text-center mb-5 text-[white] uppercase"
         >
           OUR TEAM
         </h1>
@@ -95,16 +59,16 @@
           alt=""
         />
       </div>
-      <div class="my-[100px]">
+      <div class="md:my-[100px] my-[40px]">
         <h1
-          class="text-[48px] font-bold text-center mb-5 text-[white] uppercase"
+          class="md:text-[48px] text-[36px] font-bold text-center mb-5 text-[white] uppercase"
         >
           Our address
         </h1>
         <img
           src="../assets/images/map.png"
           draggable="false"
-          class="mx-auto"
+          class="mx-auto min-h-[250px]"
           alt=""
         />
       </div>
